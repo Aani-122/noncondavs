687412566c536101ce0a0685d2a76a39c93e313f
Patil <aniket.a.patil@capgemini.com>
2023-10-06 10:12:19 +0530

    all file added

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+setup(name="project",
+      version='0.0.1',
+      author='sunny',
+      author_email='patil8624972776@gmail.com',
+      packages=find_packages(),
+      install_requires=["pandas","numpy","flask"]) 
